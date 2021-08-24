e505398c4bd0bb0108859fa2124b38139b01dc31
JChan91 <fourbin1221@gmail.com>
2021-08-24 11:01:14 +0900

    init

---
+++ primitive/boolean.js
@@ -0,0 +1,5 @@
+"use strict";
+var isBoolean = false;
+var isBoolean2 = true;
+console.log(typeof isBoolean); // boolean
+console.log(typeof isBoolean2); // boolean
---
+++ primitive/boolean.ts
@@ -0,0 +1,5 @@
+let isBoolean: boolean = false;
+let isBoolean2: Boolean = true;
+
+console.log(typeof isBoolean); // boolean
+console.log(typeof isBoolean2); // boolean
